6bd6356b7de6818482b8c663d38ad1034eeb9ae1
baraka caleb <65470823+pimby@users.noreply.github.com>
2023-10-19 13:25:02 +0300

    added redirect function  to homepage

--- dist/script.js
+++ dist/script.js
@@ -1,35 +1,62 @@
 "use strict";
-const regexKey = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-let email = document.querySelector(".email-form");
-let form = document.querySelector("form");
-let error = document.querySelector(".error");
-let header = document.querySelector("header");
-let formArea = document.querySelector("section");
-let success = document.querySelector(".success-section");
-let container = document.querySelector(".container");
-form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if ((email === null || email === void 0 ? void 0 : email.value) !== "") {
-        if (regexKey.test(email.value)) {
-            formArea.style.display = "none";
-            header.style.display = "none";
-            if (window.innerWidth >= 769) {
-                container.style.maxWidth = "375px";
+class FormValidator {
+    constructor() {
+        var _a;
+        this.regexKey = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zAZ0-9]+.)+[a-zA-Z]{2,}))$/;
+        this.email = document.querySelector(".email-form");
+        this.form = document.querySelector("form");
+        this.error = document.querySelector(".error");
+        this.header = document.querySelector("header");
+        this.formArea = document.querySelector("section");
+        this.success = document.querySelector(".success-section");
+        this.container = document.querySelector(".container");
+        (_a = this.form) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", this.handleSubmit.bind(this));
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.email &&
+            this.form &&
+            this.error &&
+            this.header &&
+            this.formArea &&
+            this.success &&
+            this.container) {
+            if (this.email.value) {
+                if (this.regexKey.test(this.email.value)) {
+                    this.formArea.style.display = "none";
+                    this.header.style.display = "none";
+                    if (window.innerWidth >= 769) {
+                        this.container.style.maxWidth = "375px";
+                    }
+                    else {
+                        this.container.style.maxWidth = "505px";
+                    }
+                    this.success.style.display = "block";
+                }
+                else {
+                    this.error.style.display = "block";
+                    this.email.style.borderColor = "#ff6257";
+                    this.email.style.color = "#ff6257";
+                    this.email.style.backgroundColor = "#ff625730";
+                }
             }
             else {
-                container.style.maxWidth = "505px";
+                this.error.style.display = "block";
+                this.container.style.width = "505px";
             }
-            success.style.display = "block";
-        }
-        else {
-            error.style.display = "block";
-            email.style.borderColor = "#ff6257";
-            email.style.color = "#ff6257";
-            email.style.backgroundColor = "#ff625730";
         }
     }
-    else {
-        error.style.display = "block";
-        container.style.width = "505px";
+}
+new FormValidator();
+class SuccessMessageHandler {
+    constructor() {
+        this.dismissButton = document.querySelector(".success-section button");
+        if (this.dismissButton) {
+            this.dismissButton.addEventListener("click", this.handleDismiss.bind(this));
+        }
     }
-});
+    handleDismiss() {
+        window.location.href = "index.html"; // Redirect to index.html
+    }
+}
+new SuccessMessageHandler();
--- src/script.ts
+++ src/script.ts
@@ -1,34 +1,74 @@
-const regexKey: RegExp =
-  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+class FormValidator {
+  private regexKey: RegExp =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zAZ0-9]+.)+[a-zA-Z]{2,}))$/;
+  private email: HTMLInputElement | null =
+    document.querySelector(".email-form");
+  private form: HTMLFormElement | null = document.querySelector("form");
+  private error: HTMLElement | null = document.querySelector(".error");
+  private header: HTMLElement | null = document.querySelector("header");
+  private formArea: HTMLElement | null = document.querySelector("section");
+  private success: HTMLElement | null =
+    document.querySelector(".success-section");
+  private container: HTMLElement | null = document.querySelector(".container");
 
-let email: HTMLInputElement | null = document.querySelector(".email-form");
-let form: HTMLFormElement | null = document.querySelector("form");
-let error: HTMLElement | null = document.querySelector(".error");
-let header: HTMLElement | null = document.querySelector("header");
-let formArea: HTMLElement | null = document.querySelector("section");
-let success: HTMLElement | null = document.querySelector(".success-section");
-let container: HTMLElement | null = document.querySelector(".container");
-
-form?.addEventListener("submit", (e: Event) => {
-  e.preventDefault();
-  if (email?.value !== "") {
-    if (regexKey.test(email!.value)) {
-      formArea!.style.display = "none";
-      header!.style.display = "none";
-      if (window.innerWidth >= 769) {
-        container!.style.maxWidth = "375px";
-      } else {
-        container!.style.maxWidth = "505px";
-      }
-      success!.style.display = "block";
-    } else {
-      error!.style.display = "block";
-      email!.style.borderColor = "#ff6257";
-      email!.style.color = "#ff6257";
-      email!.style.backgroundColor = "#ff625730";
-    }
-  } else {
-    error!.style.display = "block";
-    container!.style.width = "505px";
+  constructor() {
+    this.form?.addEventListener("submit", this.handleSubmit.bind(this));
   }
-});
+
+  private handleSubmit(e: Event) {
+    e.preventDefault();
+
+    if (
+      this.email &&
+      this.form &&
+      this.error &&
+      this.header &&
+      this.formArea &&
+      this.success &&
+      this.container
+    ) {
+      if (this.email.value) {
+        if (this.regexKey.test(this.email.value)) {
+          this.formArea.style.display = "none";
+          this.header.style.display = "none";
+          if (window.innerWidth >= 769) {
+            this.container.style.maxWidth = "375px";
+          } else {
+            this.container.style.maxWidth = "505px";
+          }
+          this.success.style.display = "block";
+        } else {
+          this.error.style.display = "block";
+          this.email.style.borderColor = "#ff6257";
+          this.email.style.color = "#ff6257";
+          this.email.style.backgroundColor = "#ff625730";
+        }
+      } else {
+        this.error.style.display = "block";
+        this.container.style.width = "505px";
+      }
+    }
+  }
+}
+
+new FormValidator();
+
+class SuccessMessageHandler {
+  private dismissButton: HTMLButtonElement | null;
+
+  constructor() {
+    this.dismissButton = document.querySelector(".success-section button");
+    if (this.dismissButton) {
+      this.dismissButton.addEventListener(
+        "click",
+        this.handleDismiss.bind(this)
+      );
+    }
+  }
+
+  private handleDismiss() {
+    window.location.href = "index.html"; // Redirect to index.html
+  }
+}
+
+new SuccessMessageHandler();
